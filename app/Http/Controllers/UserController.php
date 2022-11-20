e56e072284f3546cb964e769a1315156e3809d24
Nivra <arvinaufal23@gmail.com>
2022-11-20 14:10:52 +0700

    Update Profil Link Foto

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -106,143 +106,7 @@ class UserController extends Controller
     }
 
 
-    public function profileUpdateByIDLAMA(Request $request, $id) {
 
-        $authRole = Auth::user()->getRoleNames()[0];
-
-        if($request->has('photo')){
-            if($authRole == 'client'){
-                $userDetail = Client::where('user_id','=',$id)->first();
-                $oldImage = $userDetail->photo;
-            } elseif($authRole == 'taylor'){
-                $userDetail = Taylor::where('user_id','=',$id)->first();
-                $oldImage = $userDetail->photo;
-            } elseif($authRole == 'convection'){
-                $userDetail = Convection::where('user_id','=',$id)->first();
-                $oldImage = $userDetail->photo;
-            } elseif($authRole == 'admin'){
-                $userDetail = Admin::where('user_id','=',$id)->first();
-                $oldImage = $userDetail->photo;
-            }
-
-
-            if($oldImage){
-                $pleaseRemove = base_path('public/photos/user/').$oldImage;
-
-                if(file_exists($pleaseRemove)) {
-                    unlink($pleaseRemove);
-                }
-            }
-
-
-            $extension = $request->file('photo')->getClientOriginalExtension();
-
-            $clientPhoto = date('YmdHis').'.'.$extension;
-
-            $path = base_path('public/photos/user/');
-
-            $request->file('photo')->move($path, $clientPhoto);
-
-        }
-
-        $rules = [
-            'name'          => 'required',
-            'email'         => 'required|email|unique:users',
-
-            'phone'         => 'required',
-            'dateBirth'     => 'required',
-            'placeBirth'    => 'required',
-
-        ];
-
-        $message = [
-            'name.required'     => 'Mohon isikan nama anda',
-            'email.required'    => 'Mohon isikan email anda',
-            'email.email'       => 'Mohon isikan email valid',
-            'email.unique'      => 'Email sudah terdaftar',
-
-            'phone.required'    => 'Mohon isikan nomor hp anda',
-            'dateBirth.required'  => 'Mohon isikan tanggal lahir anda',
-            'placeBirth.required'  => 'Mohon isikan tempat lahir anda',
-
-        ];
-
-        $validator = Validator::make($request->all(), $rules, $message);
-
-        if($validator->fails()) {
-            return apiResponse(400, 'error', 'Data tidak lengkap ', $validator->errors());
-        }
-
-        try {
-            DB::transaction(function () use ($request, $id, $clientPhoto, $authRole) {
-
-
-                $user = User::where('id', $id)->first();
-                $user->update([
-                    'name'  => $request->name,
-                    'email' => $request->email,
-                    'updated_at' => date('Y-m-d H:i:s')
-                ]);
-
-
-                if($request->role == '2'){
-                    $user->syncRoles('taylor');
-                } elseif($request->role == '3') {
-                    $user->syncRoles('convection');
-                }
-
-
-            });
-
-            DB::transaction(function () use ($request, $id, $clientPhoto) {
-
-
-                $authRoleNew = Auth::user()->getRoleNames();
-
-                dd($authRoleNew);
-
-                if($authRoleNew == 'client'){
-                Client::where('user_id', $id)->update([
-                    'photo'         => $clientPhoto,
-                    'phone'         => $request->phone,
-                    'dateBirth'     => $request->dateBirth,
-                    'placeBirth'    => $request->placeBirth,
-                    'updated_at'    => date('Y-m-d H:i:s')
-                ]);
-                } elseif($authRoleNew == 'taylor'){
-                    Taylor::where('user_id', $id)->update([
-                        'photo'         => $clientPhoto,
-                        'phone'         => $request->phone,
-                        'dateBirth'     => $request->dateBirth,
-                        'placeBirth'    => $request->placeBirth,
-                        'updated_at'    => date('Y-m-d H:i:s')
-                    ]);
-                } elseif($authRoleNew == 'convection'){
-                    Convection::where('user_id', $id)->update([
-                        'photo'         => $clientPhoto,
-                        'phone'         => $request->phone,
-                        'dateBirth'     => $request->dateBirth,
-                        'placeBirth'    => $request->placeBirth,
-                        'updated_at'    => date('Y-m-d H:i:s')
-                    ]);
-                } elseif($authRoleNew == 'admin'){
-                    Admin::where('user_id', $id)->update([
-                        'photo'         => $clientPhoto,
-                        'phone'         => $request->phone,
-                        'dateBirth'     => $request->dateBirth,
-                        'placeBirth'    => $request->placeBirth,
-                        'updated_at'    => date('Y-m-d H:i:s')
-                    ]);
-                }
-
-
-            });
-
-            return apiResponse(202, 'success', 'data pengguna berhasil diperbaharui');
-        } catch(Exception $e) {
-            return apiResponse(400, 'error', 'error', $e);
-        }
-    }
 
 
 
@@ -299,7 +163,7 @@ class UserController extends Controller
 
                 $clientPhoto = date('YmdHis').'.'.$extension;
 
-                $path = base_path('public/photos/client/');
+                $path = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-client/';
 
                 $request->file('photo')->move($path, $clientPhoto);
             }
@@ -313,7 +177,7 @@ class UserController extends Controller
 
                 $taylorPhoto = date('YmdHis').'.'.$extension;
 
-                $path = base_path('public/photos/taylor/');
+                $path = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-taylor';
 
                 $request->file('photo')->move($path, $taylorPhoto);
             }
@@ -336,7 +200,7 @@ class UserController extends Controller
 
                 $convectionPhoto = date('YmdHis').'.'.$extension;
 
-                $path = base_path('public/photos/convection/');
+                $path = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-convection';
 
                 $request->file('photo')->move($path, $convectionPhoto);
             }
@@ -359,7 +223,7 @@ class UserController extends Controller
 
                 $adminPhoto = date('YmdHis').'.'.$extension;
 
-                $path = base_path('public/photos/admin/');
+                $path = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-admin';
 
                 $request->file('photo')->move($path, $adminPhoto);
             }
@@ -373,11 +237,11 @@ class UserController extends Controller
 
 
             if($oldRole == 'client') {
-                $userDetail = Taylor::where('user_id','=',$id)->first();
+                $userDetail = Client::where('user_id','=',$id)->first();
                 $oldImage = $userDetail->photo;
 
                 if($oldImage){
-                    $pleaseRemove = base_path('public/photos/taylor/').$oldImage;
+                    $pleaseRemove = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-client'.$oldImage;
 
                     if(file_exists($pleaseRemove)) {
                         unlink($pleaseRemove);
@@ -385,10 +249,40 @@ class UserController extends Controller
                 }
                 Client::where('user_id', $id)->delete();
             } elseif($oldRole == 'taylor') {
+                $userDetail = Taylor::where('user_id','=',$id)->first();
+                $oldImage = $userDetail->photo;
+
+                if($oldImage){
+                    $pleaseRemove = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-taylor'.$oldImage;
+
+                    if(file_exists($pleaseRemove)) {
+                        unlink($pleaseRemove);
+                    }
+                }
                 Taylor::where('user_id', $id)->delete();
             } elseif($oldRole == 'convection') {
+                $userDetail = Convection::where('user_id','=',$id)->first();
+                $oldImage = $userDetail->photo;
+
+                if($oldImage){
+                    $pleaseRemove = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-convection'.$oldImage;
+
+                    if(file_exists($pleaseRemove)) {
+                        unlink($pleaseRemove);
+                    }
+                }
                 Convection::where('user_id', $id)->delete();
             } elseif($oldRole == 'admin') {
+                $userDetail = Admin::where('user_id','=',$id)->first();
+                $oldImage = $userDetail->photo;
+
+                if($oldImage){
+                    $pleaseRemove = 'home/mvlrzxvo/subdomain/api.tepat.co.id/photo-admin'.$oldImage;
+
+                    if(file_exists($pleaseRemove)) {
+                        unlink($pleaseRemove);
+                    }
+                }
                 Admin::where('user_id', $id)->delete();
             }
 
